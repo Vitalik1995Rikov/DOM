f5bb696db455cd942611f1cc35ecfc762f92065b
Vitalik1995Rikov <vitalic_rikov@mail.ru>
2021-05-10 22:13:02 +0300

    1.3.1 + 1.3.2

---
+++ part1/ex_1/script.js
@@ -0,0 +1,5 @@
+console.log(document.body.children[0]); // получу div
+
+console.log(document.body.children[1]); // получу ul
+
+console.log(document.body.children[1].lastElementChild); // получу li (Пит)
---
+++ part1/ex_2/script.js
@@ -0,0 +1,3 @@
+console.log(elem.lastChild.nextSibling); // всегда равен null
+
+console.log(elem.children[0].previousSibling); // нет, перед ним может быть текстовый элемент
