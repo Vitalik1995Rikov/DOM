8bbaf08dfa4361240773b02ba30a47da97aa1c09
Vitalik1995Rikov <vitalic_rikov@mail.ru>
2021-05-11 23:00:40 +0300

    1.4

---
+++ part1/ex_4/script.js
@@ -0,0 +1,18 @@
+//1
+document.getElementById('age-table');
+
+//2
+document.getElementsByTagName('label');
+
+//3
+document.getElementByTagName('td');
+
+//4
+document.getElementsByName('search')[0];
+
+//5
+document.querySelector('input');
+
+//6
+let allInput = document.getElementsByTagName('input');
+allInput[allInput.length - 1];
