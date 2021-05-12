ed287246d280175b14af6967d8d82ac7524c2fcc
Vitalik1995Rikov <vitalic_rikov@mail.ru>
2021-05-12 21:17:27 +0300

    1.5.3 + 1.5.4

--- part1/ex_7/script.js
+++ part1/ex_7/script.js
@@ -5,3 +5,8 @@ let body = document.body;
   alert( body.firstChild.data ); // выведет BODY
   // т.к. замееняем содержимое body на "<!--" + body.tagName + "-->"
   // а body.tagName - это BODY, т.к. имя тега всегда в верхнем регистре
+
+  console.dir(document);
+  console.log(HTMLDocument.prototype.constructor.name);
+  console.log(HTMLDocument.prototype.__proto__.constructor.name);
+  console.log(HTMLDocument.prototype.__proto__.__proto__.constructor.name);
